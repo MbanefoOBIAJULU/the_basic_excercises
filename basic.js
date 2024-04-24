53d4a8d077383ef62f2075319b7b427fbf5ccb06
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-04-24 07:39:18 +0100

    .

---
+++ basic.js
@@ -0,0 +1,6 @@
+//question 1
+
+let firstName = "obiajulu" // create a string with your full name as its value
+let lastName = "dibiaezue"
+let fullName = firstName + " " + lastName //concatenate two or more strings
+console.log(fullName)
