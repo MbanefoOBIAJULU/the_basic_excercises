b713ec55dec02a19d3e71f598014217a427383a8
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-04-24 08:35:38 +0100

    .

--- basic.js
+++ basic.js
@@ -4,3 +4,13 @@ let firstName = "obiajulu" // create a string with your full name as its value
 let lastName = "dibiaezue"
 let fullName = firstName + " " + lastName //concatenate two or more strings
 console.log(fullName)
+
+//question 2
+
+let number = 4936; // declaring a number
+while (number > 0) { // printing the last digit of the number
+    const digit = number % 10; // finding thr remainder
+    console.log("Digit:", digit); // printing the remiainder
+    number = Math.floor(number / 10); // removing the last digit
+}
+ //question 3
