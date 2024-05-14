51fd2fb167777e1556d034c30c3e8e0c2517bf60
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-14 08:44:42 +0100

    .

--- task2.js
+++ task2.js
@@ -1,4 +1,8 @@
 //Create a function that will be able to convert figures from Fahrenheit to Celsius.
+function fahrenheitToCelsius(fahrenheit) {
+    var celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
 console.log(fahrenheitToCelsius(32));   // Output will be 0.0
 console.log(fahrenheitToCelsius(212));  // Output will be 100.0
 
