e4bd5f3c9df1a709e1f29e061a13794520909761
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-14 08:43:11 +0100

    .

--- task2.js
+++ task2.js
@@ -1,4 +1,6 @@
 //Create a function that will be able to convert figures from Fahrenheit to Celsius.
+console.log(fahrenheitToCelsius(32));   // Output will be 0.0
+console.log(fahrenheitToCelsius(212));  // Output will be 100.0
 
 /*Write a function min(x,y) which returns the least of two numbers x and y.
 Eg. min(6, 3) == 3*/
