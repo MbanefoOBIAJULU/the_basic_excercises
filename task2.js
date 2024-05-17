c239c1eb8255521d6d4d520af91e1a9218f4c951
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-05-17 08:30:01 +0100

    .

--- task2.js
+++ task2.js
@@ -27,9 +27,9 @@ Create a function that checks if a number, n is divisible by two numbers x and y
  function isDivisible(n, x, y) {
     return n % x === 0 && n % y === 0;
 }
-console.log(isDivisible(10, 2, 5));  // Output: true (10 is divisible by both 2 and 5)
-console.log(isDivisible(12, 3, 4));  // Output: true (12 is divisible by both 3 and 4)
-console.log(isDivisible(7, 2, 3));   // Output: false (7 is not divisible by 2 and 3)
+console.log(isDivisible(10, 2, 5));  // True (10 is divisible by 2 and 5)
+console.log(isDivisible(12, 3, 4));  // True (12 is divisible by 3 and 4)
+console.log(isDivisible(7, 2, 3));   // False (7 is not divisible 2 and 3)
 
 /*Question 6
 Create a function that will output the first 100 prime numbers.*/
